607259176f19b60ed45802f84df5fce9107894a9
sbrink00 <sbrink00@stuy.edu>
2019-02-02 09:54:31 -0500

    wrote empty methods

--- recursion.java
+++ recursion.java
@@ -1,5 +1,9 @@
 public class recursion{
   public static void main(String[]args){
-    
   }
+
+  public static double sqrt(double n, double tolerance){return 5;}
+
+  private static double sqrtHelper(double n, double guess, double tolerance){return 5;}
+
 }
