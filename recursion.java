2f1ae358aecec70bddc604df689264f12daa86a6
sbrink00 <sbrink00@stuy.edu>
2019-02-02 10:05:28 -0500

    wrote and tested sqrt method which implements the recursive sqrtHelper method

--- recursion.java
+++ recursion.java
@@ -1,12 +1,22 @@
 public class recursion{
   public static void main(String[]args){
+    System.out.println("square root helper method testing:");
     System.out.println(sqrtHelper(100, 1, .001));
-    System.out.println(sqrtHelper(.25, 400, .001));
+    System.out.println(sqrtHelper(.25, 1, .001));
     System.out.println(sqrtHelper(64, 1, .001));
-    System.out.println(sqrtHelper(2, .03, .001));
+    System.out.println(sqrtHelper(2, 1, .001));
+    System.out.println("------------------------------------------------");
+    System.out.println("square root testing. Should return the same thing as the helper method");
+    System.out.println(sqrt(100, .001));
+    System.out.println(sqrt(.25, .001));
+    System.out.println(sqrt(64, .001));
+    System.out.println(sqrt(2, .001));
+
   }
 
-  public static double sqrt(double n, double tolerance){return 5;}
+  public static double sqrt(double n, double tolerance){
+    return sqrtHelper(n, 1, tolerance);
+  }
 
   private static double sqrtHelper(double n, double guess, double tolerance){
     if (n == 0) return 0;
